3b6b94c2955677a47647a838df21da50647f00a5
fahad <fjkiani@icloud.com>
2020-03-30 23:44:21 -0400

    changes

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,8 +4,9 @@ import styles from "../css/postlist.module.css"
 const PostList = ({ posts }) => {
   return (
     <section className={styles.posts}>
-      <h1>Our Stories . Inc</h1>
+      <h1>Our Stories.Inc</h1>
       <h4>universal blog</h4>
+      <br></br>
       <div className={styles.center}>
         {posts.map(({ node }, index) => {
           return <PostCard key={index} post={node} />
