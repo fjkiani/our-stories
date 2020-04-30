e60b19c81cacd615bd64d0b175f9b8c02a0620a2
fahad <fjkiani@icloud.com>
2020-04-30 06:48:15 -0400

    adding

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,17 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "minimal-ui",
+        icon: "src/images/icon.png", 
+      },
+    },
   ],
 }
