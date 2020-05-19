7d0f8eb5f677847ed0737a2825ba445e94b35dfd
fahad <fjkiani@icloud.com>
2020-05-19 05:50:45 -0400

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,22 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  // plugins: [`gatsby-mdx`],
-
+  siteMetadata: {
+    title: `Gatsby Starter Blog`,
+    author: {
+      name: `Fahad Kiani`,
+      summary: `who lives and works in San Francisco building useful things.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    social: {
+      twitter: `kylemathews`,
+    },
+  },
   plugins: [
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `posts`,
-        path: `src/posts`,
+        path: `${__dirname}/src/posts`,
+        name: `blog`,
       },
     },
     {
@@ -32,17 +33,59 @@ module.exports = {
         },
       },
     },
+
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-166954680-1`,
+      },
+    },
+    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
-        start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
-        display: "minimal-ui",
-        // icon: "src/images/icon.png", 
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        // icon: `content/assets/gatsby-icon.png`,
       },
     },
+    `gatsby-plugin-react-helmet`,
+    // {
+    //   resolve: `gatsby-plugin-typography`,
+    //   options: {
+    //     pathToConfigModule: `src/utils/typography`,
+    //   },
+    // },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react"
 
 const counter = () => {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   return (
     <div className="counter">
       <h3>Share this</h3>
-      <h3>likes {count}</h3>
-      <button onClick={() => setCount(count + 1)}>support level</button>
+      {/* <h3>likes {count}</h3> */}
+      {/* <button onClick={() => setCount(count + 1)}>support level</button> */}
     </div>
   )
 }
