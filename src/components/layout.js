d612bee1ce51bc3f7b28048db70cbdbeaeba6fb9
fahad <fjkiani@icloud.com>
2020-06-23 01:16:28 -0400

    message

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,11 @@
 import React from "react"
 import "./layout.css"
+import Navbar from './Navbar'
+
 const layout = ({ children }) => {
   return (
     <>
+      {/* <Navbar/> */}
       <main>{children}</main>
     </>
   )
---
+++ src/pages/newsletter.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import Layout from "../components/Layout"
+
+
+// const NewsLetter = () => {
+//     return (
+//         <Layout>
+//             <section className="newsLetter-page">
+//             <div className="page-center">
+//             <h2>Get all the latest stories to your inbox</h2>
+//             </div>
+//             </section>
+//         </Layout>
+//     )
+// }
+
+// export default NewsLetter
